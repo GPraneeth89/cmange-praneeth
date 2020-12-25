50b946984399380028a4b3e7961704011e0bc089
GPraneeth89 <gpraneeth89@gmail.com>
2020-12-25 12:11:18 +0530

    crud

--- crm/urls.py
+++ crm/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.Home),
-    path('customer',views.customer),
-    path('products',views.products),
+    path('',views.Home,name="home"),
+    path('customer/<str:pk>/',views.customer,name="customer"),
+    path('products',views.products,name="products"),
+    path('create_order/',views.createOrder,name="create_order"),
+    path('update_order/<str:pk>',views.UpdateOrder,name="update_order"),
+    path('delete_order/<str:pk>',views.DeleteOrder,name="delete_order"),
 ]
--- crm/views.py
+++ crm/views.py
@@ -1,11 +1,67 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .models import *
+from .forms import *
 # Create your views here.
 from django.http import HttpResponse
+
 def Home(request):
+    orders=Order.objects.all()
+    customer=Customer.objects.all()
+    total_customers=customer.count()
+    total_orders=orders.count()
+    orders_delivered=orders.filter(status='Delivered').count()
+    orders_pending=orders.filter(status='Pending').count()
+    out_for=orders.filter(status='Out for Delivery').count()
+
+    context={'orders':orders,
+    'customer':customer,
+    'total_orders':total_orders,
+    'orders_delivered':orders_delivered,
+    'orders_pending' : orders_pending,
+    'out_for':out_for
+    }
     
-    return render(request,'crm/dashboard.html')
+    return render(request,'crm/dashboard.html',context)
 def products(request):
-    return render(request,'crm/products.html')
-def customer(request):
-    return render(request,'crm/customer.html')
+    products=Product.objects.all()
+    return render(request,'crm/products.html',{'products':products})
+def customer(request,pk):
+    customer=Customer.objects.get(id=pk)
+    
+    orders=customer.order_set.all()
+    orders_count=orders.count()
+    context={'customer':customer,'orders':orders,'oc':orders_count}
+    return render(request,'crm/customer.html',context)
+
+def createOrder(request):
+    forms=OrderForm()
+    if request.method=='POST':
+        # print('Printing POST' , request.POST)
+        forms=OrderForm(request.POST)
+        if forms.is_valid:
+            forms.save()
+            return redirect('/')
+    context={'forms':forms}
+    return render(request,'crm/order_form.html',context)
+
+def UpdateOrder(request,pk):
+    order=Order.objects.get(id=pk)
+
+    forms=OrderForm(instance=order)
+    if request.method=='POST':
+        forms=OrderForm(request.POST,instance=order)
+        if forms.is_valid:
+            forms.save()
+            return redirect('/')
+    context={'forms':forms}
+    return render(request,'crm/order_form.html',context)
+
+def DeleteOrder(request,pk):
+    order=Order.objects.get(id=pk)
+    if request.method=='POST':
+        order.delete()
+        return redirect('/')
+
+
+    context={'item':order}
+    return render(request,"crm/delete_order.html",context)
